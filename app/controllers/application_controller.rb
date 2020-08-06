ebb6b998e397022a64b11374b7ddde19ab2e634b
Jonathan East <jonathan_east@ymail.com>
2020-08-06 23:54:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,5 +19,7 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect to 'recipes/#{recipe.id}'
   end
+  
+  
 
 end
