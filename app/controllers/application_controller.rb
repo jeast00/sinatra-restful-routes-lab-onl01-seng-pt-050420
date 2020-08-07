7e01d2dff323cf680dbc914314084efc889b5055
Jonathan East <jonathan_east@ymail.com>
2020-08-07 00:24:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
   
-  post "/recipes" do
-    @recipe = Recipe.create(params)
-    redirect to 'recipes/#{recipe.id}'
-  end
-  
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
